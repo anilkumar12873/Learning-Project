c5847a54faf3a6c5a548fcf74d20c125a165bb5d
Anil Kumar <yourmail@gmail.com>
2026-06-19 19:50:17 +0530

    Testing code check-in

--- src/main/java/com/practice/Demo.java
+++ src/main/java/com/practice/Demo.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
     public static void main(String[] args) {
-
+    	//Adding change to push to test 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in/");
         driver.manage().window().maximize();
